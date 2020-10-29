2a75314dbd04cd30f33f308384a0449558b23cd8
yusuf <ysfozturk3@gmail.com>
2020-10-29 19:21:12 +0100

    solved a bug

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const resultNum = document.querySelector('#resultNum');
 const resultRoman = document.querySelector('#resultRoman');
 
 
-const numberList = { M:1000, D:500, C:100, L:50, X:10, V:5, I:1, S:0.5,}
+const numberList = { M:1000,CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1, S:0.5,}
 
 
 
